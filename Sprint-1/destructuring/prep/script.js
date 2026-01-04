7156b85a42c0e2f76ad18fd54f4d8d3064c875cb
ldfajardo10-tech <ld.fajardo10@gmail.com>
2026-01-04 16:10:27 +0100

    state rendering

--- Sprint-1/destructuring/prep/script.js
+++ Sprint-1/destructuring/prep/script.js
@@ -1,21 +1,31 @@
-const film1 = 
+const state = { // we create a const state with an object with two properties (films, searchTerm) where films is an array that contains objects with the info of each movie and the searchTerm contains a empty string.
+    films : [
 {
 title: "Killing of Flower Moon",
 director: "Martin Scorsese",
 times: ["15:35"],
 certificate: "15",
 duration: 112,
-};
-
-const film2 = {
+},
+{
 title: "Typist Artist Pirate King",
 director: "Carol Morley",
 times: ["15:00", "20:00"],
 certificate: "12A",
 duration: 108,
-};
+},
+{
+title: "the simpsons",
+director: "???????",
+times: ["15:00", "20:00"],
+certificate: "12A",
+duration: 108,
+}
+],
+    searchTerm : "",
+}
 
-function createFilmCard(film) {
+function createFilmCard(film) { // in this function we query the elements on the html and we also clone the node for the template to create film cards for each movie
 const filmCard = document
 .getElementById("film-card-template")
 .content.cloneNode(true);
@@ -24,31 +34,23 @@ filmCard.querySelector("p").textContent = film.director;
 return filmCard;
 }
 
-document.body.append(createFilmCard(film1), createFilmCard(film2));
 
-const films = [
-{
-title: "Killing of Flower Moon",
-director: "Martin Scorsese",
-times: ["15:35"],
-certificate: "15",
-duration: 112,
-},
-{
-title: "Typist Artist Pirate King",
-director: "Carol Morley",
-times: ["15:00", "20:00"],
-certificate: "12A",
-duration: 108,
-},
-];
 
-const filmCards = [];
-for (const film of films) {
-const card = createFilmCard(film);
-document.body.appendChild(card);
-filmCards.push(card);
+function render() { // we create this function to use it every time we do a different search
+    const filteredFilms = state.films.filter(function(film) { //we filter the movies to search the input value
+        return film.title.includes(state.searchTerm); // we check if the film includes the user input 
+    });
+
+    const filmCards = filteredFilms.map(createFilmCard);
+    document.getElementById("film-container").append(...filmCards);
 }
-//const filmCards = films.map(createFilmCard);
 
-document.body.append(...filmCards);
+render(); //we call our render function so we can see the list of movies when we are not searching
+
+const input = document.querySelector("input"); // we query the user input 
+input.addEventListener("keyup", function () {  // we add event listener to catch the input value
+    state.searchTerm = input.value;
+
+    document.getElementById("film-container").innerHTML = ""; // clear the previous films when searching
+    render(); // we call render so we see the filtered film on the search
+});
